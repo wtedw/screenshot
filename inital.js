61f3580b20b43b7e05205addb527dc4defe023ae
Chris <chris@DESKTOP-5VRNQK5.localdomain>
2024-06-08 15:54:21 +1000

    readfromlinks

--- inital.js
+++ inital.js
@@ -18,21 +18,45 @@ return {
 async function run() {
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash', generation_config:{"response_mime_type": "application/json"} });
 
-const prompt = "List what is the url in this chrome screenshot and provide a summary of the content\nUsing this JSON schema:\nScreenshot = {\"url\": str, \"summary\": str}\nReturn a `list[Screenshot]`"; // Use template literal for multiline string
+// const imgUrl = "https://i.imgur.com/WY5B7d3.png"
+const imgUrl = "https://stock.adobe.com/au/images/little-girl-looking-at-horse-poop-while-on-a-nature-walk/457419181"
+
+const prompt = `
+List what is the url in this screenshot and provide a summary of the content
+
+Using this JSON schema:
+
+Screenshot = {"url": str, "summary": str}
+
+Return a \`list[Screenshot]\`
+
+Given the image link
+${imgUrl}`; // Use template literal for multiline string
+
+console.log(prompt);
 
 const imageParts = [
-    fileToGenerativePart('chromess.png', 'image/png'),
+    fileToGenerativePart('chromess.png'),
     // fileToGenerativePart('image2.jpeg', 'image/jpeg'),
 ];
 
-const result = await model.generateContent([prompt, ...imageParts]);
+const result = await model.generateContent([prompt]);
+// const result = await model.generateContent([prompt, ... imageParts]);
 const response = await result.response;
 console.log(response);
 
 const text = response.text();
-
 console.log(text);
-// const url = text.
+
+// trying with the tempurature settings
+
+const cleanString = text.trim().replace(/^```json\s*|.*?```$/gm, '');
+
+console.log(cleanString);
+
+// const resJSON = JSON.parse(cleanString);
+// console.log(resJSON);
+// console.log(resJSON.url);
 }
 
 run();
